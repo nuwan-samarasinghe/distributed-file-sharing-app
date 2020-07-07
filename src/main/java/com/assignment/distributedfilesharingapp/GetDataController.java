fba723c735e8b76f01d54cc702df2dd89bf3dae0
nsamarasinghe <nuwansamarasinghe100@gmail.com>
2020-07-07 20:31:45 +0530

    adding UI changers

--- src/main/java/com/assignment/distributedfilesharingapp/GetDataController.java
+++ src/main/java/com/assignment/distributedfilesharingapp/GetDataController.java
@@ -1,6 +1,7 @@
 package com.assignment.distributedfilesharingapp;
 
 import com.assignment.distributedfilesharingapp.config.AppConfig;
+import com.assignment.distributedfilesharingapp.model.Node;
 import com.assignment.distributedfilesharingapp.model.RoutingTableDocument;
 import com.assignment.distributedfilesharingapp.model.SearchResult;
 import com.assignment.distributedfilesharingapp.service.FileService;
@@ -8,15 +9,18 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 @Slf4j
-@RestController
+@Controller
 public class GetDataController {
+
+    private final Node node;
 
     private final AppConfig appConfig;
 
@@ -24,44 +28,20 @@ public class GetDataController {
 
     private final Environment environment;
 
-    public GetDataController(AppConfig appConfig, FileService fileService, Environment environment) {
+    public GetDataController(AppConfig appConfig, FileService fileService, Environment environment, Node node) {
         this.appConfig = appConfig;
         this.fileService = fileService;
         this.environment = environment;
+        this.node = node;
     }
 
-    @GetMapping(value = "/ip-table")
-    private RoutingTableDocument printIpTable() {
-        return appConfig.getMessageBrokerThread().getRoutingTable().getRoutingTableDocument();
+    @GetMapping("/nodeui")
+    public String getNodeUI(Model model) {
+        model.addAttribute("name", node.getUserName());
+        model.addAttribute("port", node.getPort());
+        model.addAttribute("ipAddress", node.getIpAddress());
+        return "nodeui";
     }
-
-    @DeleteMapping(value = "/node")
-    private ResponseEntity deleteNode() {
-        this.appConfig.unregisterNode();
-        this.appConfig.stopApplication();
-        return ResponseEntity.ok().build();
-    }
-
-    @GetMapping(value = "/file")
-    private ResponseEntity<List<String>> getCurrentNodeFileList() {
-        log.info(this.appConfig.getFileManager().getFileNames());
-        return ResponseEntity.ok().body(this.appConfig.getFileManager().getFileNamesList());
-    }
-
-    @PostMapping(value = "/file/{fileName}")
-    private ResponseEntity<Map<String, SearchResult>> getSearchedFileList(@PathVariable("fileName") String fileName) {
-        Map<String, SearchResult> stringSearchResultMap = this.appConfig.doSearch(fileName);
-        return ResponseEntity.ok().body(stringSearchResultMap);
-    }
-
-    @PostMapping(value = "/file/download")
-    private void downloadFile(@RequestBody SearchResult searchResult) {
-        log.info("downloading the following file {}", searchResult);
-        try {
-            fileService.startReceiveFile(searchResult);
-        } catch (Exception e) {
-            log.error("an error occurred while downloading a file", e);
-        }
-    }
+    
 
 }
---
+++ src/main/java/com/assignment/distributedfilesharingapp/GetDataRestController.java
@@ -0,0 +1,65 @@
+package com.assignment.distributedfilesharingapp;
+
+import com.assignment.distributedfilesharingapp.config.AppConfig;
+import com.assignment.distributedfilesharingapp.model.RoutingTableDocument;
+import com.assignment.distributedfilesharingapp.model.SearchResult;
+import com.assignment.distributedfilesharingapp.service.FileService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@RestController
+public class GetDataRestController {
+
+    private final AppConfig appConfig;
+
+    private final FileService fileService;
+
+    private final Environment environment;
+
+    public GetDataRestController(AppConfig appConfig, FileService fileService, Environment environment) {
+        this.appConfig = appConfig;
+        this.fileService = fileService;
+        this.environment = environment;
+    }
+
+    @GetMapping(value = "/ip-table")
+    private RoutingTableDocument printIpTable() {
+        return appConfig.getMessageBrokerThread().getRoutingTable().getRoutingTableDocument();
+    }
+
+    @DeleteMapping(value = "/node")
+    private ResponseEntity deleteNode() {
+        this.appConfig.unregisterNode();
+        this.appConfig.stopApplication();
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping(value = "/file")
+    private ResponseEntity<List<String>> getCurrentNodeFileList() {
+        log.info(this.appConfig.getFileManager().getFileNames());
+        return ResponseEntity.ok().body(this.appConfig.getFileManager().getFileNamesList());
+    }
+
+    @PostMapping(value = "/file/{fileName}")
+    private ResponseEntity<Map<String, SearchResult>> getSearchedFileList(@PathVariable("fileName") String fileName) {
+        Map<String, SearchResult> stringSearchResultMap = this.appConfig.doSearch(fileName);
+        return ResponseEntity.ok().body(stringSearchResultMap);
+    }
+
+    @PostMapping(value = "/file/download")
+    private void downloadFile(@RequestBody SearchResult searchResult) {
+        log.info("downloading the following file {}", searchResult);
+        try {
+            fileService.startReceiveFile(searchResult);
+        } catch (Exception e) {
+            log.error("an error occurred while downloading a file", e);
+        }
+    }
+
+}
