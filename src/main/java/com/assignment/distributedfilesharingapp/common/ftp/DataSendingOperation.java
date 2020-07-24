8b9c63d657e6b9b993de0298ad2d91128cd32630
nsamarasinghe <nuwansamarasinghe100@gmail.com>
2020-07-24 13:49:16 +0530

    adding dynamic file content change & sha generation

--- src/main/java/com/assignment/distributedfilesharingapp/common/ftp/DataSendingOperation.java
+++ src/main/java/com/assignment/distributedfilesharingapp/common/ftp/DataSendingOperation.java
@@ -3,9 +3,11 @@ package com.assignment.distributedfilesharingapp.common.ftp;
 import com.assignment.distributedfilesharingapp.common.FileManager;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.env.Environment;
+import sun.misc.BASE64Encoder;
 
 import java.io.*;
 import java.net.Socket;
+import java.security.MessageDigest;
 
 @Slf4j
 public class DataSendingOperation implements Runnable {
@@ -26,28 +28,50 @@ public class DataSendingOperation implements Runnable {
             DataInputStream dIn = new DataInputStream(clientSocket.getInputStream());
             String fileName = dIn.readUTF();
             FileManager fileManager = FileManager.getInstance(userName, this.environment.getProperty("app.common.file-name"));
-            sendFile(fileManager.getFile(fileName));
+            File file = fileManager.getFile(fileName);
+            sendFile(file, updateFileContent(file));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    private Long updateFileContent(File file) throws IOException {
+        log.info("update the file content {}", file.getAbsolutePath());
+        long randomNum;
+        do {
+            randomNum = (long) (Math.random() * 10);
+        } while (randomNum == 0);
+        log.info("generated file size is {} bites", randomNum * 1024 * 1024);
+        RandomAccessFile randomAccessFile = new RandomAccessFile(file, "rw");
+        randomAccessFile.setLength(randomNum * 1024 * 1024);
+        randomAccessFile.close();
+        return randomNum;
+    }
 
-    public void sendFile(File file) {
+
+    public void sendFile(File file, Long fileSize) {
         try {
             //handle file read
-            byte[] myByTearray = new byte[(int) file.length()];
+            byte[] buffer = new byte[(int) file.length()];
+            int count;
             FileInputStream fis = new FileInputStream(file);
             BufferedInputStream bis = new BufferedInputStream(fis);
             DataInputStream dis = new DataInputStream(bis);
-            dis.readFully(myByTearray, 0, myByTearray.length);
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            dis.readFully(buffer, 0, buffer.length);
+            while ((count = bis.read(buffer)) > 0) {
+                digest.update(buffer, 0, count);
+            }
+            byte[] hash = digest.digest();
+            log.info("following is the SHA encoded file {} file size {}", new BASE64Encoder().encode(hash), fileSize);
+            bis.close();
             //handle file send over socket
             OutputStream os = clientSocket.getOutputStream();
             //Sending file name and file size to the server
             DataOutputStream dos = new DataOutputStream(os);
             dos.writeUTF(file.getName());
-            dos.writeLong(myByTearray.length);
-            dos.write(myByTearray, 0, myByTearray.length);
+            dos.writeLong(buffer.length);
+            dos.write(buffer, 0, buffer.length);
             dos.flush();
             fis.close();
             log.info("File " + file.getName() + " sent to client.");
